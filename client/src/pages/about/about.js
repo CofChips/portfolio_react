aff600f59211f5ab0eae20e05a42d9177c2480ee
CofChips <qiwei.mod@gmail.com>
2020-08-15 16:18:31 -0700

    created files for footer and pages

---
+++ client/src/pages/about/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.css";
+import Navbar from "../../components/Navbar/index";
+import Header from "../../components/Header/index";
+
+function About() {
+    return (
+        <div>
+            <Navbar />
+            <Header header={"Who I am"} />
+        </div>
+
+    )
+}
+
+export default About;
---
+++ client/src/pages/contact/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./about.css";
+import Navbar from "../../components/Navbar/index";
+import Header from "../../components/Header/index";
+
+function Contact() {
+    return (
+        <div>
+            <Navbar />
+            <Header header={"Who I am"} />
+        </div>
+
+    )
+}
+
+export default Contact;
