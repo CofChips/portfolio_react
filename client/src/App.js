b422f3cd4c02d7c14ec5f4fd354f646c6a283b99
CofChips <qiwei.mod@gmail.com>
2020-08-15 16:16:56 -0700

    setup boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import About from "../src/pages/about/about";
+
+function App() {
+  return (
+    <div>
+<About/>
+    </div>
+  );
+}
+
+export default App;
